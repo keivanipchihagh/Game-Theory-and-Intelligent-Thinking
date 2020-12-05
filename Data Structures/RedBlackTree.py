5434de058809e1b1c7420c33cbabbd73be714290
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2020-12-05 18:18:49 +0330

    Update RedBlackTree.py

--- Data Structures/RedBlackTree.py
+++ Data Structures/RedBlackTree.py
@@ -5,16 +5,22 @@ Creation data:		Monday, November 8, 2020
 GitHub:				https://github.com/keivanipchihagh/Game-Theory-and-Intelligent-Thinking/blob/main/Data%20Structures/RedBlackTree.py
 '''
 
+# Import BinarySearchTree.py
+# import importlib
+# importlib.import_module('BinarySearchTree')
+from BinarySearchTree import BinarySearchTree
+
 class Node:
 	''' Node object contains a value, a pointers to left and a pointer to the right node '''
 
 	def __init__(self, value):
 		''' Construction - Initializes the object variables '''
 
-		self.right = self.left = self.parent = self.color = None
+		self.right = self.left = self.parent = None
+		self.color = 'RED'
 		self.value = value
 
-class RedBlackTree:
+class RedBlackTree(BinarySearchTree):
 	''' 
 									~ Red Black Tree (Python 3 edition) ~
 
@@ -56,6 +62,8 @@ class RedBlackTree:
 	def __init__(self):
 		''' Construction - Initializes the object variables to default values '''
 
+		super().__init__()
+
 		# Default values
 		self.root = None
 		self.size = self.height = 0
@@ -84,10 +92,7 @@ class RedBlackTree:
 		if parent is None:
 			return None
 
-		if parent.left == node:
-			return parent.right
-		else:
-			return parent.left
+		return parent.right if (parent.left == node) else parent.left		
 
 
 	def get_uncle(self, node):
@@ -95,41 +100,6 @@ class RedBlackTree:
 
 		parent = self.get_parent(node)
 		return None if (parent is None) else self.get_sibling(parent)
-
-
-	def is_subtree(self, tree):
-		''' Checks if the given BST tree is the subtree of the current BST tree '''
-
-		def is_subarray(self, arr_1, arr_2):
-
-			i = j = 0
-
-			while i < len(arr_1) and j < len(arr_2):
-
-				if arr_1[i] == arr_2[j]:
-					i += 1
-					j += 1
-
-					# Second tree traverse completed
-					if j == len(arr_2):
-						return True
-
-				else:
-					i = i - j + 1
-					j = 0
-
-			return False
-
-		# Get 'In Order' & 'Pre Order' of tree_1
-		tree_1_in_order = self.in_order_traverse()
-		tree_1_pre_order = self.pre_order_traverse()
-
-		# Get 'In Order' & 'Pre Order' of tree_2
-		tree_2_in_order = tree.in_order_traverse()
-		tree_2_pre_order = tree.pre_order_traverse()
-
-		# Compare arrays for each tree respectively
-		return is_subarray(self, tree_1_in_order, tree_2_in_order) and is_subarray(self, tree_1_pre_order, tree_2_pre_order)
 
 
 	def print(self, new_line = True, reversed = False):
@@ -143,7 +113,7 @@ class RedBlackTree:
 				return
 
 			traverse(self, iterator.right, reversed) if (reversed == True) else traverse(self, iterator.left, reversed)
-			print(iterator.value, iterator.color, sep = '-', end = ' ')
+			print(iterator.value, iterator.color[0], sep = '-', end = ' ')
 			traverse(self, iterator.left, reversed) if (reversed == True) else traverse(self, iterator.right, reversed)
 
 		iterator = self.root
@@ -167,7 +137,7 @@ class RedBlackTree:
 			in_order_traverse_recursive(self, iterator.left, array)
 
 			if no_print == False:
-				print(iterator.value, iterator.color, sep = '-', end = ' ')
+				print(iterator.value, iterator.color[0], sep = '-', end = ' ')
 			array.append(iterator.value)
 			in_order_traverse_recursive(self, iterator.right, array)
 
@@ -185,7 +155,7 @@ class RedBlackTree:
 				# Backtrack the stack
 				elif stack:
 					iterator = stack.pop()	# Get the last node
-					print(iterator.value, iterator.color, sep = '-', end = ' ')
+					print(iterator.value, iterator.color[0], sep = '-', end = ' ')
 					array.append(iterator.value)
 					iterator = iterator.right
 				else:
@@ -204,6 +174,62 @@ class RedBlackTree:
 		return array
 
 
+	def rotate_left(self, node):
+		''' Rotates the given value to the left '''
+
+		iterator = self.root
+
+		# In case value does not exist
+		if node is None or node.right is None:
+			return
+
+		temp = node.right
+		node.right = temp.left
+
+		if temp.left is not None:
+			temp.left.parent = node
+
+		temp.parent = node.parent
+
+		if node.parent is None:
+			self.root = temp
+		elif node is node.parent.left:
+			node.parent.left = temp
+		else:
+			node.parent.right = temp
+
+		temp.left = node
+		node.parent = temp			
+
+
+	def rotate_right(self, node):
+		''' Rotates the given value to the right '''
+
+		iterator = self.root
+
+		# In case value does not exist
+		if node is None or node.left is None:
+			return
+
+		temp = node.left
+		node.left = temp.right
+
+		if temp.right is not None:
+			temp.right.parent = node
+
+		temp.parent = node.parent
+
+		if node.parent is None:
+			self.root = temp
+		elif node is node.parent.right:
+			node.parent.right = temp
+		else:
+			node.parent.left = temp
+
+		temp.right = node
+		node.parent = temp	
+
+
 	def pre_order_traverse(self, new_line = True, no_print = False):
 		''' Traverse the list pre order '''
 
@@ -215,7 +241,7 @@ class RedBlackTree:
     				return
 
 			if no_print == False:
-				print(iterator.value, iterator.color, sep = '-', end = ' ')
+				print(iterator.value, iterator.color[0], sep = '-', end = ' ')
 			array.append(iterator.value)
 			pre_order_traverse_recursive(self, iterator.left, array)
 			pre_order_traverse_recursive(self, iterator.right, array)
@@ -228,7 +254,7 @@ class RedBlackTree:
 			while True:
 
 				if iterator is not None:
-					print(iterator.value, iterator.color, sep = '-', end = ' ')
+					print(iterator.value, iterator.color[0], sep = '-', end = ' ')
 					array.append(iterator.value)
 					stack.append(iterator)		
 					iterator = iterator.left
@@ -267,7 +293,7 @@ class RedBlackTree:
 			post_order_traverse_recursive(self, iterator.left, array)
 			post_order_traverse_recursive(self, iterator.right, array)
 			if no_print == False:
-				print(iterator.value, iterator.color, sep = '-', end = ' ')
+				print(iterator.value, iterator.color[0], sep = '-', end = ' ')
 			array.append(iterator.value)
 
 		def post_order_traverse_none_recursive(self, iterator, array):
@@ -293,7 +319,7 @@ class RedBlackTree:
 						continue
 
 					last_out = stack.pop()
-					print(last_out.value, iterator.color, sep = '-', end = ' ')
+					print(last_out.value, iterator.color[0], sep = '-', end = ' ')
 					array.append(iterator.value)
 
 		iterator = self.root
@@ -307,110 +333,7 @@ class RedBlackTree:
 
 		# Returns the array if needed
 		return array
-
-
-	def exists(self, value):
-		''' Check whether the given value exists in the tree or not '''
-
-		if self.size == 0: raise Exception('Tree is empty!')
-
-		iterator = self.root
-		return True if (self.get_node(value, iterator) == True) else False
-
-
-	def get_node(self, value, iterator):
-		'''  Returns the node containing the given value '''
-
-		# Exit condition
-		if iterator is None:
-			return iterator
-
-		if value == iterator.value:
-			return iterator
-		elif value < iterator.value:
-			return self.get_node(value, iterator.left)
-		else:
-			return self.get_node(value, iterator.right)
-
-
-	def get_min(self, value, get_node = False):
-		''' Get the minimum value from a given parent value '''
-
-		iterator = self.root
-		iterator = self.get_node(value, iterator)	# Get the corresponding node
-
-		while iterator.left is not None:
-			iterator = iterator.left
-
-		# Return node or its value
-		return iterator if (get_node == True) else iterator.value
-
-
-	def get_max(self, value, get_node = False):
-		''' Get the maxumum value from a given parent value '''
-
-		iterator = self.root
-		iterator = self.get_node(value, iterator)	# Get the corresponding node
-
-		while iterator.right is not None:
-			iterator = iterator.right
-
-		return iterator if (get_node == True) else iterator.value
-
-
-	def get_successor(self, value, get_node = False):
-		''' Get the successor for the given value '''
-
-		iterator = self.root
-		node = self.get_node(value, iterator)	# Get the corresponding node
-
-		if node.right is not None:
-   			return self.get_min(node.right.value)
-
-		parent = node.parent
-		while parent is not None and parent.right == node:
-			node = parent
-			parent = parent.parent	
-
-		# Return node or its value
-		return parent if (get_node == True) else parent.value
-
-
-	def get_predecessor(self, value, get_node = False):
-		''' Get the predecessor for the given value '''
-
-		iterator = self.root
-		node = self.get_node(value, iterator)	# Get the corresponding node
-
-		if node.left is not None:
-			return self.get_max(node.left.value)
-
-		parent = node.parent
-		while parent is not None and parent.left == node:
-			node = parent
-			parent = parent.parent	
-
-		# Return node or its value
-		return parent if (get_node == True) else parent.value
-
-
-	def get_depth(self):
-		''' Returns the depth of the tree '''
-
-		def get_depth(self, node):
-			''' Recursivly get the depth of the tree '''
-
-			if node is None:
-				return -1
-
-			max_left = get_depth(self, node.left)
-			max_right = get_depth(self, node.right)
-
-			return max_left + 1 if (max_left > max_right) else max_right + 1
-
-		node = self.root
-		return get_depth(self, node)
-
+	
   
 	def fancy_print(self) : 
 		''' Fancy prints the tree '''
@@ -432,70 +355,14 @@ class RedBlackTree:
   
 			# Print current node after space  
 			# count  
-			print('\n', abs(space - COUNT[0]) * ' ', node.value + '-' + node.color, sep = '')
+			print('\n', abs(space - COUNT[0]) * ' ', str(node.value) + '-' + node.color[0], sep = '')
 	  
 			# Process left branch  
 			fancy_print(self, node.left, space)    
 
 		temp = self.root
 		fancy_print(self, node = temp, space = 0)
-		print()
-
-
-	def rotate_left(self, node):
-		''' Rotates the given value to the left '''
-
-		iterator = self.root
-
-		# In case value does not exist
-		if node is None or node.right is None:
-			return
-
-		temp = node.right
-		node.right = temp.left
-
-		if temp.left is not None:
-			temp.left.parent = node
-
-		temp.parent = node.parent
-
-		if node.parent is None:
-			self.root = temp
-		elif node is node.parent.left:
-			node.parent.left = temp
-		else:
-			node.parent.right = temp
-
-		temp.left = node
-		node.parent = temp			
-
-
-	def rotate_right(self, node):
-		''' Rotates the given node to the right '''
-
-		iterator = self.root
-
-		# In case value does not exist
-		if node is None or node.left is None:
-			return
-
-		temp = node.left
-		node.left = temp.right
-
-		if temp.right is not None:
-			temp.right.parent = node
-
-		temp.parent = node.parent
-
-		if node.parent is None:
-			self.root = temp
-		elif node is node.parent.right:
-			node.parent.right = temp
-		else:
-			node.parent.left = temp
-
-		temp.right = node
-		node.parent = temp
+		print()	
 
 
 	def insert(self, value):
@@ -508,7 +375,7 @@ class RedBlackTree:
 				# left branch
 				if node.value < iterator.value:					
 					if iterator.left is not None:
-						insert_recurse(iterator.left, node)		# Go deeper to the left branch
+						insert_recurse(self, iterator.left, node)		# Go deeper to the left branch
 						return
 					else:
 						iterator.left = node
@@ -516,7 +383,7 @@ class RedBlackTree:
 				# Right branch (Can contain duplicates)
 				else:
 					if iterator.right is not None:
-						insert_recurse(iterator.right, node)	# Go deeper to the right branch
+						insert_recurse(self, iterator.right, node)	# Go deeper to the right branch
 						return
 					else:
 						iterator.right = node
@@ -525,37 +392,62 @@ class RedBlackTree:
 			node.left = node.right = None	# NIL children
 			node.color = 'RED'				# Set as 'RED', it violates rules 2 & 3, but it's easy to fix
 
-		def insert_fix(self, node):
-			''' Fixex the tree using the 3 cases for insertion '''
+			insert_fix_case_1(self, node)
 
-			parent = self.get_parent(node)
-			uncle = self.get_uncle(node)
+		# def insert_fix(self, node):
+		# 	''' Fixex the tree using the 3 cases for insertion '''
 
-			# Case 1
-			if parent is None:
-				insert_fix_case_1(self, node)
+		# 	parent = self.get_parent(node)
+		# 	uncle = self.get_uncle(node)
 
-			# Case 2
-			elif uncle.color == 'RED':
-				insert_fix_case_2(self, node)
+		# 	# Case 1
+		# 	if parent is None:
+		# 		insert_fix_case_1(self, node)
 
-			# Case 3 & 4
-			elif uncle.color == 'BLACK' and parent.color == 'RED':
-				insert_fix_case_3(self, node)
+		# 	# Case 2
+		# 	elif uncle is not None and uncle.color == 'RED':
+		# 		insert_fix_case_2(self, node)
+
+		# 	# Case 3 & 4
+		# 	elif uncle is not None and uncle.color == 'BLACK' and parent.color == 'RED':
+  #   				insert_fix_case_3(self, node)
 
 		def insert_fix_case_1(self, node):
 			''' In this case, the node is the root. Solution: color it BLACK '''
 
 			# 1. Recolor
-			node.color = 'BLACK'
-	
+			if node.parent is None:
+				node.color = 'BLACK'
+			else:
+				insert_fix_case_1_5(self, node)
+
+
+		def insert_fix_case_1_5(self, node):
+			'''  In this case, the new node has a black parent. All the properties are still satisfied and we return '''
+				
+			parent = self.get_parent(node)
+
+			if parent.color == 'BLACK':
+				return
+			else:
+				insert_fix_case_2(self, node)
+
+
 		def insert_fix_case_2(self, node):
 			''' In this case, the node's uncle and parent are RED. Solution: revert their colors respectively '''
 
+			uncle = self.get_uncle(node)
+
 			# 1. Recolor
-			self.get_parent(node).color = 'RED'
-			self.get_uncle(node).color = 'RED'
-			self.get_grand_parent(node).color = 'BLACK'
+			if uncle is not None and uncle.color == 'RED':
+				self.get_parent(node).color = 'BLACK'
+				self.get_uncle(node).color = 'BLACK'
+				self.get_grand_parent(node).color = 'RED'
+
+				insert_fix_case_1(self, self.get_grand_parent(node))		# We might mess up the upper nodes, so recheck recursively
+			else:
+				insert_fix_case_3(self, node)
+
 
 		def insert_fix_case_3(self, node):
 			''' In this case, the node's uncle is BLACK and the error shapes a triangle '''
@@ -570,9 +462,8 @@ class RedBlackTree:
 			elif node == parent.left and parent == grand_parent.right:
 				self.rotate_right(parent)
 				node = node.right
-
-			# Now case 4 occures
-			insert_fix_case_4(self, node)
+			
+			insert_fix_case_4(self, node)	# We might mess up the upper nodes, so recheck recursively
 
 		def insert_fix_case_4(self, node):
 			''' In this case, the node's uncle us BLACK and the error shapes a line '''
@@ -581,7 +472,7 @@ class RedBlackTree:
 			grand_parent = self.get_grand_parent(node)
 
 			# 1. Rotate
-			if node == parent.left:
+			if node == parent.left and parent == grand_parent.left:
 				self.rotate_right(grand_parent)
 			else:
 				self.rotate_left(grand_parent)
@@ -594,11 +485,15 @@ class RedBlackTree:
 		iterator = self.root
 		node = Node(value)
 
+		# In case the root is empty
+		if self.root is None:
+			self.root = node
+
 		# Insert the node to the current tree
 		insert_recurse(self, iterator, node)
 
 		# Repair the tree in using the 3 cases if any of the rules are violated
-		insert_fix(self, node)
+		# insert_fix(self, node)
 
 
 	def insert_range(self, ls):
@@ -607,10 +502,17 @@ class RedBlackTree:
 		for item in ls:
 			self.Insert(item)
 
-RB = RedBlackTree()
-RB.insert(15)
-RB.in_order_traverse()
 
+# Initialization
+RB = RedBlackTree()
+
+# Insert
 RB.insert(5)
-RB.in_order_traverse()
-# RB.fancy_print()
+RB.insert(10)
+RB.insert(3)
+RB.insert(1)
+RB.insert(9)
+RB.insert(2)
+RB.insert(4)
+RB.insert(8)
+RB.fancy_print()
