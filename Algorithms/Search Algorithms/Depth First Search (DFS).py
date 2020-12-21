bca7d335fe98e5a1b0edee48efc5bbe57039f098
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2020-12-21 16:45:42 +0330

    Update Depth First Search (DFS).py

--- Algorithms/Search Algorithms/Depth First Search (DFS).py
+++ Algorithms/Search Algorithms/Depth First Search (DFS).py
@@ -20,54 +20,40 @@ class Graph:
 
 		self.graph[edge] = nodes
 
+	def Depth_First_Search(self, vertex, visited):
+		''' Recursive PFS algorithm '''
+
+		# Mark vertex as visited
+		visited.add(vertex)
+
+		# Print edges value
+		print(vertex, end = ' ')
+
+		# Check all adjacent vertecies of the vertex
+		for adjacent in self.graph[vertex]:
+
+			if adjacent not in visited:
+				self.Depth_First_Search(adjacent, visited)
+
+
 	def DFS(self, vertex):
 		''' Depth First Search Algortihm - Time complexity: O(V + E) where V is the number of vertices and E is the number of edges in the graph '''
-
-		def DFS(self, vertex, visited):
-			''' Recursive PFS algorithm '''
-
-			# Mark vertex as visited
-			visited.add(vertex)
-
-			# Print edges value
-			print(vertex, end = ' ')
-
-			# Check all adjacent vertecies of the vertex
-			for adjacent in self.graph[vertex]:
-
-				if adjacent not in visited:
-					DFS(self, adjacent, visited)
 
 		# Create a list to store visited status (Add one extra False for the vertex itself)
 		visited = set()
 
-		DFS(self, vertex, visited)
+		self.Depth_First_Search(vertex, visited)
 		print()
 
 	def DFS_all(self):
 		''' Depth First Search Algortihm - Time complexity: O(V + E) where V is the number of vertices and E is the number of edges in the graph '''
-
-		def DFS(self, vertex, visited):
-			''' Recursive PFS algorithm '''
-
-			# Mark vertex as visited
-			visited.add(vertex)
-
-			# Print edges value
-			print(vertex, end = ' ')
-
-			# Check all adjacent vertecies of the vertex
-			for adjacent in self.graph[vertex]:
-
-				if adjacent not in visited:
-					DFS(self, adjacent, visited)
 
 		# Create a list to store visited status (Add one extra False for the vertex itself)
 		visited = set()
 
 		for vertex in self.graph.keys():
 			if vertex not in visited:
-				DFS(self, vertex, visited)
+				self.Depth_First_Search(vertex, visited)
 
 		print()
 
